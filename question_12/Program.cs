6368b13b8f2fccdf7ba5503a7f47934b8c658d4b
YuuSatou19970726 <44957064+YuuSatou19970726@users.noreply.github.com>
2023-02-17 20:33:46 +0700

    push question_12 (T6 17/02/2023)

---
+++ question_12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace question_12
+{
+    public delegate void MyDelegate(string message);
+
+    public class Messenger
+    {
+        public event MyDelegate MessageSent;
+
+        public void SendMessage(string message)
+        {
+            Console.WriteLine("Sending message: " + message);
+            if (MessageSent != null)
+            {
+                MessageSent(message);
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void OnMessageSent(string massage)
+        {
+            Console.WriteLine("Message sent: " + massage);
+        }
+
+        static void Main(string[] args)
+        {
+            Messenger messenger = new Messenger();
+            messenger.MessageSent += OnMessageSent;
+            messenger.SendMessage("Hello World!");
+        }
+    }
+}

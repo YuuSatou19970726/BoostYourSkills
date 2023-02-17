23fc52557c0dd626534f181d604c3469641a11e4
YuuSatou19970726 <44957064+YuuSatou19970726@users.noreply.github.com>
2023-02-17 19:33:14 +0700

    push question_11 (T6 17/02/2023)

---
+++ question_11/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace question_11
+{
+    public delegate void Value(int x, int y);
+
+    internal class Program
+    {
+
+        static void Add(int x, int y)
+        {
+            Console.WriteLine(x + " + " + y + " = " + (x + y));
+        }
+
+        static void Sub(int x, int y)
+        {
+            Console.WriteLine(x + " - " + y + " = " + (x - y));
+        }
+
+        static void Mul(int x, int y)
+        {
+            Console.WriteLine(x + " * " + y + " = " + (x * y));
+        }
+
+        static void Div(int x, int y)
+        {
+            Console.WriteLine(x + " / " + y + " = " + (x / y));
+        }
+
+        static void Main(string[] args)
+        {
+            Value value = new Value(Add);
+            value += Sub;
+            value += Mul;
+            value += Div;
+
+            value(4, 2);
+        }
+    }
+}

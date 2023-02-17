c209b31752e3ea412e63ec6e181f31ce25eb6523
YuuSatou19970726 <44957064+YuuSatou19970726@users.noreply.github.com>
2023-02-17 19:30:19 +0700

    push question_9 (T6 17/02/2023)

---
+++ question_9/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace question_9
+{
+    //tạo 1 delegate nhận 1 tham số kiểu int, kiểu trả về: void
+    public delegate void IntAction(int x);
+
+    internal class Program
+    {
+
+        public class Counter
+        {
+            //khởi tạo event tương ứng với delegate
+            public event IntAction Counted;
+
+
+            public void Count(int n)
+            {
+                for(int i = 0; i < n; i++)
+                {
+                    Console.WriteLine(i);
+                    //khởi chạy event IntAction có nhiệm vụ lặp lại Count 1 lần
+                    Counted?.Invoke(i);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Counter counter = new Counter();
+            //
+            counter.Counted += delegate (int x)
+            {
+                Console.WriteLine(x);
+            };
+            counter.Count(5);
+        }
+    }
+}
